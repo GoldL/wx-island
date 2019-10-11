6ee47363724931bfd9e54b52804607b597de7e6a
iGolden <136895062@qq.com>
2019-10-12 00:01:56 +0800

    完成音乐播放逻辑

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,26 +1,79 @@
 import {
   classicBeh
 } from '../classic-beh'
+
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
    */
   behaviors: [classicBeh],
   properties: {
-
+    src: String,
+    title: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    playing: false,
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png',
+  },
 
+  attached(event) {
+    // 跳转页面 当前 切换
+    this._recoverStatus()
+    this._monitorSwitch()
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay() {
+      // 图片要切换
+      if (!this.data.playing) {
+        this.setData({
+          playing: true
+        })
+        mMgr.src = this.properties.src
+        mMgr.title = this.properties.title
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    },
+    _recoverStatus() {
+      if (mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return
+      }
+      if (mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
+      }
+    },
+    _monitorSwitch() {
+      mMgr.onPlay(() => {
+        this._recoverStatus()
+      })
+      mMgr.onPause(() => {
+        this._recoverStatus()
+      })
+      mMgr.onStop(() => {
+        this._recoverStatus()
+      })
+      mMgr.onEnded(() => {
+        this._recoverStatus()
+      })
+    }
   }
 })
